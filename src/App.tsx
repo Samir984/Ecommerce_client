e3820a1c78d5c4c6bbfd3cd8e9fba9ad39fd023b
samir984 <sasuki984@gmail.com>
2024-02-15 08:34:48 +0545

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <div>clean slite
+    <Button variant={"default"}>click me</Button>
+  </div>;
+}
+
+export default App;

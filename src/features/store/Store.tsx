0f58fd3ba38740a013881e5811047947be393f04
samir984 <sasuki984@gmail.com>
2024-04-14 22:45:52 +0545

    feature:implemented signout

--- src/features/store/Store.tsx
+++ src/features/store/Store.tsx
@@ -13,7 +13,7 @@ export default function Store() {
   console.log(store);
   return (
     <div className="py-4 ">
-      {!store_id ? (
+      {store_id ? (
         <StoreNotFound />
       ) : (
         <div className="flex flex-col h-full">
@@ -28,7 +28,7 @@ export default function Store() {
           )}
 
           <div className="flex-grow min-h-[800px]">
-            <StoreProducts store_id={store_id} />
+            <StoreProducts store_id={store_id as string} />
           </div>
         </div>
       )}
--- src/features/store/useStoreData.ts
+++ src/features/store/useStoreData.ts
@@ -5,8 +5,8 @@ export function useStoreData(store_id: string) {
   const { isLoading: isLoadingStore, data: store } = useQuery({
     queryKey: ["store"],
     queryFn: () => fetchStoreData(store_id),
-    
+    retry: false,
   });
   console.log("useStoreData running");
-  return {isLoadingStore, store};
+  return { isLoadingStore, store };
 }

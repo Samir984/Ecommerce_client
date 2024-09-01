76ca58fb10503ee838b725600cccd39e82f833db
Samir Neupane <106372023+Samir984@users.noreply.github.com>
2024-09-01 12:38:20 +0545

    Update SubCategory.tsx

--- src/components/SubCategory.tsx
+++ src/components/SubCategory.tsx
@@ -18,42 +18,39 @@ export default function SubCategory() {
   console.log(subCategories, isLoading);
 
   return (
-    <div className="flex overflow-y-scroll hidescroll_bar gap-3  py-4 tablet:px-2 tablet:min-h-48 min-h-32 laptop:max-w-[1300px] mx-auto ">
-      {isLoading ? (
-        <div className="flex gap-4">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, i) => {
-            return (
+    <div className="flex flex-col items-center tablet:px-4 mx-auto py-6 laptop:max-w-[1300px]">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Categories</h1>
+      <div className="flex overflow-y-scroll hidescroll_bar gap-6 py-4 min-h-32 tablet:min-h-48">
+        {isLoading ? (
+          <div className="flex gap-4">
+            {[...Array(12)].map((_, i) => (
               <div className="flex flex-col gap-2 items-center" key={i}>
                 <Skeleton className="w-18 h-18 tablet:w-24 tablet:h-24 rounded-full" />
-                <div className="space-y-2">
-                  <Skeleton className="h-4 w-[80px]" />
-                </div>
+                <Skeleton className="h-4 w-[80px]" />
               </div>
-            );
-          })}
-        </div>
-      ) : (
-        subCategories.data.map((subCategory: SubCategory) => {
-          return (
+            ))}
+          </div>
+        ) : (
+          subCategories.data.map((subCategory: SubCategory) => (
             <Link
               to={`results?subcategory=${subCategory._id}`}
-              className="flex flex-col flex-shrink-0 items-center gap-3"
+              className="flex flex-col flex-shrink-0 items-center gap-3 transition-transform duration-200 hover:scale-105"
               key={subCategory._id}
             >
-              <div className="p-1 bg-gray-100  rounded-full hover:bg-gray-200 ">
+              <div className="p-2 bg-gray-100 rounded-full hover:bg-gray-200">
                 <img
                   src={subCategory.url}
                   alt={subCategory._id}
                   className="w-16 h-16 tablet:w-24 tablet:h-24 rounded-full"
                 />
               </div>
-              <span className="text-gray-700 font-mono ">
+              <span className="text-gray-700 font-mono">
                 {subCategory._id}
               </span>
             </Link>
-          );
-        })
-      )}
+          ))
+        )}
+      </div>
     </div>
   );
 }
